e04c421b1857050a8c315f530883eea0ec680560
Dan Croak <dcroak@thoughtbot.com>
2010-06-05 02:15:11 +0800

    fixing syntax error in install.rb

--- install.rb
+++ install.rb
@@ -3,5 +3,5 @@ path = "config/routes.rb"
 
 puts "inserting #{line} into #{path}"
 
-content = File.read(path).gsub(/^(class|module|.*Routes).*$/, {|match| "#{match}\n  #{line}" })
+content = File.read(path).gsub(/^(class|module|.*Routes).*$/) {|match| "#{match}\n  #{line}" }
 File.open(path, 'wb') { |file| file.write(content) }
