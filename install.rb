7f4e231db281cea9d44bcee5c2818d0f3bb03824
Dan Croak <dcroak@thoughtbot.com>
2010-06-05 02:52:24 +0800

    sub (not gsub) in case of multiple matches

--- install.rb
+++ install.rb
@@ -3,5 +3,5 @@ path = "config/routes.rb"
 
 puts "inserting #{line} into #{path}"
 
-content = File.read(path).gsub(/^(end\s*)$/) {|match| "  #{line}\n#{match}" }
+content = File.read(path).sub(/^(end\s*)$/) {|match| "  #{line}\n#{match}" }
 File.open(path, 'wb') { |file| file.write(content) }
