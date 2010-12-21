ec12cd1a8a38b84bb0f212af86d94aa1c5e047b4
Matt Jankowski <mjankowski@thoughtbot.com>
2010-12-21 09:44:40 -0500

    update test helper to use dummy app structure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,15 @@ ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
 
-require 'shoulda'
-require 'shoulda/rails'
+require "shoulda"
+require "shoulda/rails"
+
+Rails.backtrace_cleaner.remove_silencers!
 
 begin require 'redgreen'; rescue LoadError; end
+
+# Run any available migration
+ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
