a971da64d38f0ff0f294c5ff3ab77be28534e779
Matt Jankowski <mjankowski@thoughtbot.com>
2010-12-30 10:51:21 -0500

    Remove files that rails3 apps dont need - we have a separate branch for pre-3.0 apps

--- init.rb
+++
@@ -1,1 +0,0 @@
-require 'high_voltage'
--- install.rb
+++
@@ -1,7 +0,0 @@
-line = "HighVoltage::Routes.draw(map)"
-path = "config/routes.rb"
-
-puts "inserting #{line} into #{path}"
-
-content = File.read(path).sub(/^(end\s*)$/) {|match| "  #{line}\n#{match}" }
-File.open(path, 'wb') { |file| file.write(content) }
