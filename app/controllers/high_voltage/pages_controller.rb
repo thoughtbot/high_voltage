2fd01b96dba20fbe3a27df2b1502e1f855010950
Xavier Spriet <xavier@netmon.ca>
2011-09-13 00:30:23 -0400

    Enable high_voltage config through an initializer, and provide a handful of settings

--- app/controllers/high_voltage/pages_controller.rb
+++ app/controllers/high_voltage/pages_controller.rb
@@ -1,27 +1,29 @@
 class HighVoltage::PagesController < ApplicationController
-
+  caches_page *HighVoltage::caches_page
   unloadable
 
   rescue_from ActionView::MissingTemplate do |exception|
     if exception.message =~ %r{Missing template pages/}
-      raise ActionController::RoutingError, "No such page: #{params[:id]}"
+      raise ActionController::RoutingError, "No such page: #{params[:action]}"
     else
       raise exception
     end
   end
 
-  def show
+  def method_missing(name, *args, &block)
+    puts "Fuuuuu! #{name}"
     render :template => current_page
   end
 
   protected
 
     def current_page
-      "pages/#{clean_path}"
+      "#{HighVoltage::content_path}#{clean_path}"
     end
 
     def clean_path
-      path = Pathname.new "/#{params[:id]}"
+      puts "Params: #{params.inspect} - Action: #{params[:action]}"
+      path = Pathname.new "/#{params[:action]}"
       path.cleanpath.to_s[1..-1]
     end
 
--- lib/high_voltage.rb
+++ lib/high_voltage.rb
@@ -1,3 +1,16 @@
 module HighVoltage
+  mattr_accessor :caches_page
+  @@caches_page = nil
+
+  mattr_accessor :layout
+  @@layout = "application"
+
+  mattr_accessor :content_path
+  @@content_path = "pages/"
+
+  def self.setup
+    yield self
+  end
+
   require 'high_voltage/engine' if defined?(Rails)
 end
