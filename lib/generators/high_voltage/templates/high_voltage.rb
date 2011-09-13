d6d3c32fe0bef5bd55313fec95c1795a621cc2cb
Xavier Spriet <xavier@netmon.ca>
2011-09-13 01:40:46 -0400

    Clean up initializer template

--- lib/generators/high_voltage/templates/high_voltage.rb
+++ lib/generators/high_voltage/templates/high_voltage.rb
@@ -1,9 +1,9 @@
 HighVoltage.setup do |config|
   # List of pages that should be cached.
-  config.caches_page = *[:test, :ladeedah]
+  #config.caches_page = *[]
 
   # Location of your static content
-  config.content_path = 'pages/'
+  #config.content_path = 'pages/'
 
   # Layout that will be used to render your pages
   #config.layout = 'application'
