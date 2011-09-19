634cbbca97762c6f50f15a2a0826bf34be97fbad
Xavier Spriet <xavier@netmon.ca>
2011-09-19 10:11:20 -0400

    Use HighVoltage::content_path for generated routes too

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,29 +1,62 @@
 require 'spec_helper'
-
 describe 'routes' do
+  context "using default configuration" do
 
-  it "should generate normal resource route with id" do
-    page_path(:id => "one").should == "/pages/one"
+    it "should generate normal resource route with id" do
+      page_path(:id => "one").should == "/pages/one"
+    end
+
+    it "should generate normal resource route with string" do
+      page_path("one").should == "/pages/one"
+    end
+
+    it "should generate nested route with string" do
+      page_path("one/two").should == "/pages/one/two"
+    end
+
+    it "should recognize nested route" do
+      assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one/two" }, "/pages/one/two")
+    end
+
+    it "should recognize normal route" do
+      assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one" }, "/pages/one")
+    end
+
+    it "should recognize normal route with dots" do
+      assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one.two.three" }, "/pages/one.two.three")
+    end
   end
 
-  it "should generate normal resource route with string" do
-    page_path("one").should == "/pages/one"
-  end
+  context "using a custom content_path" do
 
-  it "should generate nested route with string" do
-    page_path("one/two").should == "/pages/one/two"
-  end
+    before(:all) do
+      HighVoltage::content_path = "other_pages/"
+      Rails.application.reload_routes!
+    end
 
-  it "should recognize nested route" do
-    assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one/two" }, "/pages/one/two")
-  end
+    it "should generate normal resource route with id" do
+      page_path(:id => "one").should == "/other_pages/one"
+    end
 
-  it "should recognize normal route" do
-    assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one" }, "/pages/one")
-  end
+    it "should generate normal resource route with string" do
+      page_path("one").should == "/other_pages/one"
+    end
 
-  it "should recognize normal route with dots" do
-    assert_recognizes({ :controller => "high_voltage/pages", :action => "show", :id => "one.two.three" }, "/pages/one.two.three")
+    it "should generate nested route with string" do
+      page_path("one/two").should == "/other_pages/one/two"
+    end
+
+    it "should recognize nested route" do
+      assert_recognizes({:controller => "high_voltage/pages", :action => "show", :id => "one/two"}, "/other_pages/one/two")
+    end
+
+    it "should recognize normal route" do
+      assert_recognizes({:controller => "high_voltage/pages", :action => "show", :id => "one"}, "/other_pages/one")
+    end
+
+    it "should recognize normal route with dots" do
+      assert_recognizes({:controller => "high_voltage/pages", :action => "show", :id => "one.two.three"}, "/other_pages/one.two.three")
+    end
   end
 
 end
