31570f91cb5a19a147b8bf4e4423db5102918b00
Odin Dutton <odindutton@gmail.com>
2012-07-06 15:44:29 +1000

    Add a config option to disable default routes.
    If you want more control over your routing you can disable the default
    routes in an initializer:
    
        HighVoltage.no_routes = true

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  match "/#{HighVoltage.content_path}*id" => 'high_voltage/pages#show', :as => :page, :format => false
-
+  unless HighVoltage.no_routes
+    match "/#{HighVoltage.content_path}*id" => 'high_voltage/pages#show', :as => :page, :format => false
+  end
 end
