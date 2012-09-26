2899ce4b0aad27a5672a36a4607e5ccea8f9d230
Mike Burns <mike@mike-burns.com>
2012-09-26 10:58:42 +0200

    Remove the ActionMailer usage

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "action_mailer/railtie"
 
 Bundler.require
 require "high_voltage"
