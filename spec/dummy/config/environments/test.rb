4af1310ce17a6e507fd30f559b707eb9304700a4
Matt Jankowski <mjankowski@thoughtbot.com>
2013-03-01 20:14:33 -0800

    Set eager load false to avoid rails4 warning

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -7,6 +7,8 @@ Dummy::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
+  config.eager_load = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
