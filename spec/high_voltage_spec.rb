95003c44ef34e2dcf00df8f253fdca4d901ad318
linduxed <linduxed@gmail.com>
2013-05-07 19:36:14 +0200

    Made the main library file loadable.
    high_voltage.rb uses mattr_accessor methods, which are provided by
    active_support. Normally these methods are provided by having Rails ran
    beforehand, but this meant that high_voltage couldn't be loaded alone.
    
    Added necessary require to address this.

--- spec/high_voltage_spec.rb
+++ spec/high_voltage_spec.rb
@@ -1,7 +1,11 @@
-require 'spec_helper'
+require 'minimal_spec_helper'
 
 describe HighVoltage do
   it 'should be valid' do
     HighVoltage.should be_a(Module)
+  end
+
+  it 'should be loadable without preloading rails' do
+    expect { require 'high_voltage' }.not_to raise_error
   end
 end
