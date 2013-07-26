334a2c1fa116d36fed45bbb8322529c8d428a298
Arsen Gasparyan <becode21@gmail.com>
2013-07-26 16:27:36 +0400

    Rails 3 path hook

--- lib/high_voltage/engine.rb
+++ lib/high_voltage/engine.rb
@@ -1,4 +1,8 @@
 module HighVoltage
   class Engine < Rails::Engine
+    initializer "Require concern path" do |app|
+      concern_path = "app/controllers/concerns" 
+      app.paths.add(concern_path) unless app.paths.keys.include?(concern_path)
+    end
   end
 end
