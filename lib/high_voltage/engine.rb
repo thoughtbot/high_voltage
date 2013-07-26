d879fd06b1916fa26e1bc3dfda11e5aa6184853b
Arsen Gasparyan <becode21@gmail.com>
2013-07-26 17:07:34 +0400

    require concern for rails 3 or lower

--- lib/high_voltage/engine.rb
+++ lib/high_voltage/engine.rb
@@ -3,6 +3,10 @@ module HighVoltage
     initializer "Require concern path" do |app|
       concern_path = "app/controllers/concerns" 
       app.paths.add(concern_path) unless app.paths.keys.include?(concern_path)
+      require "concerns/high_voltage/pageable.rb"
     end
   end
 end
+
+
+  
