8f548afcde5f8393a87054eb414507120cc4129c
Arsen Gasparyan <becode21@gmail.com>
2013-07-26 16:17:34 +0400

    Move PagesController code to concern

--- spec/controllers/action_caching_controller_spec.rb
+++ spec/controllers/action_caching_controller_spec.rb
@@ -2,25 +2,22 @@ require 'spec_helper'
 
 describe HighVoltage::PagesController, 'action_caching' do
   it 'caches the action when action_caching is set to true', enable_caching: true do
-    HighVoltage.action_caching = true
-
-    load('high_voltage/pages_controller.rb')
-    get :show, id: 'exists'
-
-    action_was_cached(:exists).should be_true
+    expect {
+      HighVoltage.action_caching = true
+      concern_reload
+      get :show, id: :exists
+   }.to change { action_was_cached(:exists) }
   end
 
   it 'does not cache the action when action_caching is set to false', enable_caching: true do
-    HighVoltage.action_caching = false
-
-    load('high_voltage/pages_controller.rb')
-    get :show, id: 'exists'
-
-    action_was_cached(:exists).should be_false
+    expect {
+      HighVoltage.action_caching = false
+      concern_reload
+      get :show, id: :exists
+    }.to_not change { action_was_cached(:exists) }
   end
 
   def action_was_cached(page)
     ActionController::Base.cache_store.exist?("views/test.host#{page_path(page)}")
   end
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,15 @@ RSpec.configure do |config|
   config.include RSpec::Matchers
   config.mock_with :rspec
 end
+
+def concern_reload
+  HighVoltage::PagesController.class_eval do
+    if respond_to?(:caches_action)
+      caches_action :show, if: -> { HighVoltage.action_caching }
+    end
+
+    if respond_to?(:caches_page)
+      caches_page :show, if: -> { HighVoltage.page_caching }
+    end
+  end
+end
