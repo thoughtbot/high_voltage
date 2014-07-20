1f727ac742e8d07837a076695ad301e9f5a48f16
Harlow Ward <harlow@hoteltonight.com>
2014-07-20 11:55:34 -0700

    Add require statements for Rails 3
    We've been seeing some issues with Rails 3 applications unable to find
    the `StaticPage` module.
    
    * Add custom require for Rails 3
    * Update quotes to match new style guide
    
    https://github.com/thoughtbot/high_voltage/issues/146

--- lib/high_voltage/engine.rb
+++ lib/high_voltage/engine.rb
@@ -1,14 +1,21 @@
 module HighVoltage
   class Engine < Rails::Engine
-    initializer 'Set up default parent engine' do |app|
+    initializer "Set up default parent engine" do |app|
       HighVoltage.parent_engine ||= Rails.application
     end
 
-    initializer 'Require concerns path' do |app|
-      concerns_path = 'app/controllers/concerns'
+    initializer "Require concerns path" do |app|
+      concerns_path = "app/controllers/concerns"
 
       unless app.paths.keys.include?(concerns_path)
         app.paths.add(concerns_path)
+      end
+    end
+
+    initializer "Require for Rails 3" do |app|
+      if defined?(Rails) && Rails::VERSION::MAJOR == 3
+        require "concerns/high_voltage/static_page"
+        require "high_voltage/pages_controller"
       end
     end
   end
